ee824a9824064f6af314ab3a7a664a2076958c4a
dolceficante <john.dolce@MacBookAir-johndolce.local>
2016-04-07 15:55:04 -0500

    Call service & display json

--- src/Controller/SCRecommenderController.php
+++ src/Controller/SCRecommenderController.php
@@ -8,12 +8,20 @@
 namespace Drupal\sc_recommender\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use GuzzleHttp\Client;
 
 class SCRecommenderController extends ControllerBase {
   public function content() {
+    $client = new Client();
+    $response = $client->get('http://localhost:8080/recommender/coleparmer/json/', array(
+      'headers' => array('Accept' => 'application/hal+json')
+    ));
+    $json = $response->getBody();
+    //print_r($response->json());
     return array(
         '#type' => 'markup',
-        '#markup' => $this->t('Hello, World!'),
+        //'#markup' => $this->t('Hello, World!'),
+        '#markup' => $json,
     );
   }
 }
